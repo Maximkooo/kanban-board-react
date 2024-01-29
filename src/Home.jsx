499cd0ad83a1514aab9f791d21a9d5f6318db6df
VladysslavTkach <tkachacc.program@gmail.com>
2024-01-29 17:28:01 +0100

    footer update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,13 @@
-import './App.scss'
+import "./App.scss";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+	return (
+		<>
+			<Footer />
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
