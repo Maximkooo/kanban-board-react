448067ea7c56c2ec29de8d16738f6d28fbc3bb1f
Maksym Synytskyi <citomax7196@gmail.com>
2024-01-29 17:41:44 +0100

    navbar block

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Navbar.scss'
+import logo from '../../assets/ironSpace-logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <a href='#'>
+        <img src={logo} className="" alt="IronSpace" />
+      </a>
+      <h6 className='font-bold text-3xl'>Kanban Board</h6>
+    </div>
+
   )
 }
 export default Navbar
