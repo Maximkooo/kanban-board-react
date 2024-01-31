160e8168ddb16eee20521cb6bc2367a87e0b8d63
Maksym Synytskyi <citomax7196@gmail.com>
2024-01-31 16:23:53 +0100

    kanban board

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,11 @@
 import './Home.scss'
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar'
-// import Sidebar from './components/Sidebar'
+
 
 function App() {
   return (
     <>
-    <h1>hello</h1>
       <Navbar />
       <Dashboard/>
     </>
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,24 +1,16 @@
 import './Dashboard.scss'
 import React from 'react'
 import Sidebar from '../Sidebar'
-import DashboardList from './DashboardList'
-
-import { TASKS, STATUSES } from '../../common/constants'
-
+import Kanban from '../Kanban'
 
 const Dashboard = () => {
-  const filterOfStatusHandler = (status) => {
-    return TASKS.filter((task) => task.status === status)
-  }
   return (
     <div className='dashboard__container'>
       <Sidebar />
       <div className='dashboard__template'>
         <div className='pb-5'>FILTER</div>
         <div className='dashboard__statuses'>
-          {STATUSES.map((status, idx) => (
-            <DashboardList key={idx} status={status} tasks={filterOfStatusHandler(status)} />
-          ))}
+          <Kanban />
         </div>
       </div>
     </div>
