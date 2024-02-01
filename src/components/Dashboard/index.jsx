f32c0404226f7b56fbf1e92e32a37ca08a53286b
Maksym Synytskyi <citomax7196@gmail.com>
2024-02-01 10:47:03 +0100

    filter UI

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -2,13 +2,16 @@ import './Dashboard.scss'
 import React from 'react'
 import Sidebar from '../Sidebar'
 import Kanban from '../Kanban'
+import FilterCards from '../FilterCards'
 
 const Dashboard = () => {
   return (
     <div className='dashboard__container'>
       <Sidebar />
       <div className='dashboard__template'>
-        <div className='pb-5'>FILTER</div>
+        <div className='pb-5'>
+          <FilterCards />
+        </div>
         <div className='dashboard__statuses'>
           <Kanban />
         </div>
