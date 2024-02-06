ba90ba90a674413007875f039b9be20ee19e2b95
Maksym Synytskyi <citomax7196@gmail.com>
2024-02-06 09:37:58 +0100

    logo link to dashboard

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
 	return (
 		<div className="navbar">
-			<Link to="/">
+			<Link to="/dashboard">
 				<img src={logo} className="inline-block mt-2" alt="IronSpace" />
 				<img src={reactLogo} className="inline-block ml-1" alt="React" />
 			</Link>
